7b1edb3a60b4ae4b34051ea5adda07718ae7cb37
unknown <jhanishitback@gmail.com>
2022-01-14 16:57:13 +0530

    Added some stuff to the popup but stuff is still remaining

--- website/pages/index.js
+++ website/pages/index.js
@@ -2,6 +2,7 @@ import { useSession, signIn } from "next-auth/client";
 import { useEffect, useState } from "react";
 
 import Layout from "../components/Layout";
+import Login from "../components/Login";
 import { Popup, useOnClickOutside } from "../components/Popup";
 import styles from "../styles/pages/index.module.scss";
 
@@ -15,17 +16,8 @@ export default function App() {
     return (
       <Layout>
         <button onClick={() => setPopupState(true)}>Get Started</button>
-        <Popup popupState={popupState} ref={popupRef} center>
-          <h1 className={styles["popup__login__heading"]}>I am a...</h1>
-
-          <div className={styles["popup__login__container"]}>
-            <button className={styles["popup__login__container__button"]}>
-              Oompa Loompa
-            </button>
-            <button className={styles["popup__login__container__button"]}>
-              Customer
-            </button>
-          </div>
+        <Popup popupState={popupState} ref={popupRef} center id={styles["login-popup"]}>
+          <Login setPopupState={setPopupState}/>
         </Popup>
       </Layout>
     );
