412a11fa61a79aeaeb2fd76f2bc3e84099208c70
YashLovesCode <yashtri22@gmail.com>
2022-01-14 23:32:11 +0530

    hell0

--- website/components/SideNav/index.js
+++ website/components/SideNav/index.js
@@ -1,7 +1,7 @@
 import styles from "./index.module.scss";
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes, faQrcode, faLink, faStream, faCalendarWeek, faQuestionCircle , faSlidersH, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots, faUser, faCreditCard, faHome} from '@fortawesome/free-solid-svg-icons'
 export default function SideNav() {
   const [isOpen, setOpen] = useState(true);
 
@@ -11,19 +11,16 @@ export default function SideNav() {
   <header>  Wonka's</header>
   <ul className={styles.ul}>
     <li>
-      <a href="#"><FontAwesomeIcon icon={faQrcode}></FontAwesomeIcon>Home</a>
+      <a href="#"><FontAwesomeIcon icon={faHome}></FontAwesomeIcon>Home</a>
     </li>
     <li>
-      <a href="#"><FontAwesomeIcon icon={faLink}></FontAwesomeIcon>Chat</a>
+      <a href="#"><FontAwesomeIcon icon={faCommentDots}></FontAwesomeIcon>Chat</a>
     </li>
     <li>
-      <a href="#"><FontAwesomeIcon icon={faStream}></FontAwesomeIcon>Paycheck</a>
+      <a href="#"><FontAwesomeIcon icon={faCreditCard} ></FontAwesomeIcon>Paycheck</a>
     </li>
     <li>
-      <a href="#"><FontAwesomeIcon icon={faCalendarWeek}></FontAwesomeIcon>Profile</a>
-    </li>
-    <li>
-      <a href="#"><FontAwesomeIcon icon={faQuestionCircle} ></FontAwesomeIcon>About</a>
+      <a href="#"><FontAwesomeIcon icon={faUser} ></FontAwesomeIcon>Profile</a>
     </li>
   </ul>
   </div>
