33c59d43ed9102c233808a72dcdb389b6ccc4a7e
Aahan Agarwal <aahanagarwal87@gmail.com>
2022-01-16 00:30:11 +0530

    /manager/items

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -10,12 +10,22 @@ router.get("/", auth, async (req, res) => {
   }
 
   const user = await User.findOne({ _id: req.user.id });
-  if (user.role !== "customer") {
-    return res.send({ success: false, message: "Malformed Request" });
-  }
 
   const products = await Product.find({});
   return res.send({ success: true, products: products });
+});
+
+router.post("/delete", auth, async (req, res) => {
+  if (!req.user) {
+    return res.send({ success: false, message: "Invalid Credentials" });
+  }
+
+  const user = await User.findOne({ _id: req.user.id });
+  if (user.role !== "oompaloompa" || user.isManager !== true) {
+    return res.send({ success: false, message: "Unauthorized" });
+  }
+
+  await Product.findOneAndDelete({ _id: req.body.id });
 });
 
 router.post("/", auth, async (req, res) => {
