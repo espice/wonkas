f307620aa9cdc17347ddab2c92d949c2924a2180
Arnav Bansal <arnavbansal101@gmail.com>
2022-01-16 00:29:11 +0530

    add oompa popup basic done

--- website/pages/manager/index.js
+++ website/pages/manager/index.js
@@ -2,21 +2,34 @@ import Layout from "../../components/Layout";
 import Sidebar from "../../components/SideNav";
 import { FAB } from "../../components/Button";
 import PlusIcon from "../../public/icons/plus.svg";
+import { Popup, useOnClickOutside } from "../../components/Popup";
 
 import styles from "../../styles/pages/manager/home.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import UserContext from "../../components/userContext";
 
 const Manager = () => {
   const { user } = useContext(UserContext);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const popupRef = useRef();
+
+  useOnClickOutside(popupRef, () => setPopupOpen(false));
+
+  const NewOompaPopup = () => {
+    return (
+      <div className={styles["new-loompa-popup"]}>
+        <PlusIcon />
+      </div>
+    );
+  };
 
   return (
     <Layout>
       <Sidebar />
-      <FAB>
+      <FAB onClick={() => setPopupOpen(true)}>
         <PlusIcon />
       </FAB>
       <div className={styles.main}>
@@ -25,6 +38,9 @@ const Manager = () => {
         </h1>
         <h4 className={styles["main__sub-heading"]}>Manage Oompa Loompas</h4>
       </div>
+      <Popup ref={popupRef} popupState={popupOpen}>
+        <NewOompaPopup />
+      </Popup>
     </Layout>
   );
 };
