f20a94555bb70a0ef329e84e2bc81c200f1e6371
Aahan Agarwal <aahanagarwal87@gmail.com>
2022-01-17 12:07:12 +0530

    Add paycheck schema

--- server/src/models/paycheck.js
+++ server/src/models/paycheck.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
 
 const paycheckSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Types.ObjectId,
-        required: [true, "User is missing"]
-    },
-    amount: {
-        type: Number,
-        default: 100,
-        required: [true, "Amount is missing"]
-    },
-    time: {
-        type: Date,
-        required: [true, "Time is missing"]
-    }
+  user: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "User is missing"],
+  },
+  amount: {
+    type: Number,
+    default: 100,
+    required: [true, "Amount is missing"],
+  },
+  nextPayday: {
+    type: Date,
+    required: [true, "Payday is missing"],
+  },
+  salaryHistory: {
+    type: [
+      {
+        amount: Number,
+        date: Date,
+        isCollected: Boolean,
+      },
+    ],
+  },
 });
 
 const Paycheck = mongoose.model("Paychecks", paycheckSchema);
