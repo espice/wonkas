588d5b2cbd3f4778329fb2ad83023036302b1a30
YashLovesCode <yashtri22@gmail.com>
2022-01-17 20:13:29 +0530

    kill me v2

--- website/components/PaycheckPayPopup/index.js
+++ website/components/PaycheckPayPopup/index.js
@@ -1,16 +1,28 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "../../config/axios"
 
 const PayCheckPopup = (userId, oldVals) => {
     console.log(userId)
+    console.log(oldVals)
     const [payment, setPayment] = useState(150)
-    oldVals.forEach(key => {
+    const [total, setTotal] = useState(0)
+    useEffect(() => {
+        axios.get("/api/total/").then((value) => {
+            console.log(value.data.total.amount)
+            setTotal(value.data.total.amount)
+        })
+    }, [])
+    userId.oldVals.forEach(key => {
         if (new Date(oldVals["date"]).getTime() <= new Date().getTime()) {
             setPayment(payment + oldVals["amount"])
         }
     })
     function submitHandler () {
-        
+        axios.post("/api/total/update", {
+            amount: total - payment
+        }).then((value) => {
+            console.log(value)
+        })
     }
     return (
     <div>
