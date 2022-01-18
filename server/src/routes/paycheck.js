91589ee6652b95ea7d7b055a4887f77af31632b0
Haunter-123 <jhanishitback@gmail.com>
2022-01-18 09:36:00 +0530

    payment init

--- server/src/routes/paycheck.js
+++ server/src/routes/paycheck.js
@@ -1,26 +1,28 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const Paycheck = require("@models/paycheck");
-const User = require("@models/user");
+const auth = require("@middleware/auth");
+
 const router = Router();
 
-router.get("/", async (req, res) => {
-    const oompaPayCheck = await Paycheck.findOne({user: req.params.id});
-    res.send({success: true, oompaPayCheck});
-})
+router.get("/", auth, async (req, res) => {
+  const oompaPayCheck = await Paycheck.findOne({ user: req.user.id });
+  res.send({ success: true, paycheck: oompaPayCheck });
+});
 router.get("/all", async (req, res) => {
-    const allPaychecks = await Paycheck.find({}).populate({
-        path: "user",
-        model: "User",
-    })
-    res.send({success: true, allPaychecks});
-})
+  const allPaychecks = await Paycheck.find({}).populate({
+    path: "user",
+    model: "User",
+    select: "name email isManager photoUrl role _id",
+  });
+  res.send({ success: true, allPaychecks });
+});
 
-router.get("/update", async (req, res) => {
-    const paycheck = await Paycheck.findOne({user: req.params.id});
-    paycheck.amount = req.body.amount;
-    
-    await paycheck.save();
-    res.send({success: true, paycheck});
-})
+router.get("/update", auth, async (req, res) => {
+  const paycheck = await Paycheck.findOneAndUpdate(
+    { user: req.user.id },
+    { amount: req.body.amount }
+  );
+  res.send({ success: true, paycheck });
+});
 
-module.exports = router
+module.exports = router;
---
+++ website/pages/manager/payments/index.js
@@ -0,0 +1,156 @@
+import Layout from "../../../components/Layout";
+import axios from "../../../config/axios";
+import SideNav from "../../../components/SideNav";
+import PayCheckPopup from "../../../components/PaycheckPayPopup";
+import { Popup, useOnClickOutside } from "../../../components/Popup";
+import styles from "./index.module.scss";
+import { useState, useEffect, useRef } from "react";
+import BeanIcon from "../../../public/icons/bean.svg";
+import classNames from "classnames/bind";
+import { formatLong } from "../../../config/dates";
+const cx = classNames.bind(styles);
+const Paychecks = () => {
+  const [paychecks, setPaychecks] = useState([]);
+  const [editPayCheck, setEditPayCheck] = useState({});
+  const [total, setTotal] = useState(0);
+  const [buttonUser, setButtonUser] = useState({});
+  const [old, setOld] = useState({});
+  const [isPayPopupOpen, setIsPayPopupOpen] = useState(false);
+  const popupRef = useRef(null);
+
+  useOnClickOutside(popupRef, () => {
+    setIsPayPopupOpen(false);
+  });
+
+  useEffect(async () => {
+    const response = await axios.get("/api/paycheck/all");
+    const data = response.data.allPaychecks;
+
+    setPaychecks(data);
+
+    const totalReq = await axios.get("/api/total");
+    const totalData = totalReq.data.total;
+    setTotal(totalData.amount);
+  }, []);
+
+  function handlePayCheckAdd(e, amount, id) {
+    e.preventDefault();
+    console.log(amount);
+  }
+  return (
+    <Layout>
+      <SideNav />
+      <div className={styles.main}>
+        <div className={styles.main__header}>
+          <div className={styles.main__header__title}>
+            <h1 className={styles.main__header__title__heading}>Paychecks</h1>
+            <p className={styles.main__header__title__description}>
+              Pay Oompa Loompas
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.main__content}>
+          <div className={styles.main__content__headrow}>
+            <p className={styles.main__content__headrow__label}>Budget:</p>
+
+            <div className={styles.main__content__headrow__actionlabel}>
+              <p className={styles.main__content__headrow__actionlabel__label}>
+                {total}
+              </p>
+              <BeanIcon />
+            </div>
+          </div>
+
+          <div className={styles.main__content__category}>
+            {paychecks.map((value, i) => {
+              return (
+                <div className={styles.main__content__category__row}>
+                  <div className={styles.main__content__category__row__info}>
+                    <p
+                      className={
+                        styles.main__content__category__row__info__label
+                      }
+                    >
+                      {value.user.name}
+                    </p>
+
+                    <p
+                      className={
+                        styles.main__content__category__row__info__info
+                      }
+                    >
+                      {new Date(value.nextPaycheck).getTime() <=
+                      new Date().getTime()
+                        ? "Salary Due!"
+                        : `Salary Due on ${formatLong(value.nextPaycheck)}`}
+                    </p>
+                  </div>
+
+                  <div className={styles.main__content__category__row__actions}>
+                    <button
+                      className={cx({
+                        main__content__category__row__actions__button: true,
+                        "button-primary": true,
+                      })}
+                    >
+                      View History
+                    </button>
+                    <button
+                      className={cx({
+                        main__content__category__row__actions__button: true,
+                        "button-primary": true,
+                      })}
+                      onClick={(e) => {
+                        setIsPayPopupOpen(true), setButtonUser(value.user);
+                      }}
+                    >
+                      Pay Now
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <Popup popupState={isPayPopupOpen} ref={popupRef} center>
+          <PayCheckPopup userId={buttonUser} />
+        </Popup>
+      </div>
+    </Layout>
+  );
+};
+export default Paychecks;
+
+/*
+{paychecks.map((value, i) => {
+          return (
+            <div>
+              <h1> Name: {value.user.name}</h1>
+              <h3>Salary: {value.salary}</h3>
+              {new Date(value.nextPaycheck).getTime() <=
+              new Date().getTime() ? (
+                <h4>payment due</h4>
+              ) : (
+                <h4>payment not due</h4>
+              )}
+              {console.log(
+                new Date(value.nextPaycheck).getTime() > new Date().getTime()
+              )}
+              <button
+                onClick={(e) => {
+                  setIsPayPopupOpen(true),
+                    setButtonUser(value.user),
+                    setOld(value.paycheckHistory);
+                }}
+              >
+                pay this gareeban aadmi pls bro pls
+              </button>
+              <button>
+                iski history dekho search waali nahi warna momy scold
+              </button>
+            </div>
+          );
+        })}
+        */
